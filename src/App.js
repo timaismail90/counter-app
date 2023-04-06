021b5fb1bf7e653815d3eba786fc93118cb4da55
timaismail90 <timaismail90@gmail.com>
2023-04-06 14:39:18 -0400

    created counter app that uses javascript, react, css, html, and tailwind

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment, reset } from "./redux/slice";
 
-function App() {
+const styles = {
+  margin: "2px",
+};
+
+const App = () => {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> Counter : {count}</h1>
+      <button style={styles} onClick={() => dispatch(increment())}>
+        Increase Counter
+      </button>
+      <button style={styles} onClick={() => dispatch(decrement())}>
+        Decrease Counter
+      </button>
+      <button style={styles} onClick={() => dispatch(reset())}>
+        Reset Counter
+      </button>
     </div>
   );
-}
+};
 
 export default App;
